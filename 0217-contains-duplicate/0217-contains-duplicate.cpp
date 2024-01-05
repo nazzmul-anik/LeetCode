cffbddeb28f7321faed850de8bbbf120ea7079a7
Md. Nazmul Hasan Anik <110289715+nazzmul-anik@users.noreply.github.com>
2024-01-06 01:15:14 +0600

    Time: 141 ms (23.17%), Space: 71.1 MB (27.09%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,12 +3,12 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         map<int, int> mp;
         for(auto num : nums){
+            
+            if(mp[num]>=1) return true;
+
             mp[num]++;
         }
-
-        for(auto it : mp){
-            if(it.second > 1) return true;
-        }
+    
         return false;
     }
 };
