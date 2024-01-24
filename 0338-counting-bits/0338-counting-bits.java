83786417bf0bf1fe6f6d12e89808f7e56551b305
Md. Nazmul Hasan Anik <110289715+nazzmul-anik@users.noreply.github.com>
2024-01-25 00:18:40 +0600

    Time: 3 ms (41.56%), Space: 46.8 MB (28.85%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,19 +1,20 @@
 class Solution {
 
-    public static int totalOne(int n){
+    int countOne(int n){
         int one=0;
-        while(n>0){
+        while(n!=0){
             one++;
             n = n & (n-1);
         }
+
         return one;
     }
 
     public int[] countBits(int n) {
         
-        int[] ans = new int[n+1];
+        int[] ans  = new int[n+1];
         for(int i=0; i<=n; i++){
-            ans[i] = totalOne(i);
+            ans[i] = countOne(i);
         }
 
         return ans;
