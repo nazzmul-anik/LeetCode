6fa16b3d1eb196572707cc55149dd7dd700aee4b
Md. Nazmul Hasan Anik <110289715+nazzmul-anik@users.noreply.github.com>
2024-03-06 09:03:18 +0600

    Time: 0 ms (100%), Space: 39.9 MB (99.81%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,24 +1,24 @@
 class Solution {
 
-    public static int solve(int target, int[] nums, int[] dp){
-        if(target < 0) return 0;
+    int solve(int n, int target, int[] nums, int[] dp){
         if(target == 0) return 1;
+        if(target < 0) return 0;
+
         if(dp[target] != -1) return dp[target];
 
         int sum = 0;
-        for(int i=0; i<nums.length; i++){
-            sum = sum + solve(target-nums[i], nums, dp);
+        for(int i=0; i<n; i++){
+            sum = sum + solve(n, target - nums[i], nums, dp);
         }
-
         return dp[target] = sum;
     }
 
     public int combinationSum4(int[] nums, int target) {
+        int n = nums.length;
         int[] dp = new int[target+1];
         for(int i=0; i<=target; i++){
             dp[i] = -1;
         }
-
-        return solve(target, nums, dp);
+        return solve(n, target, nums, dp);
     }
 }
