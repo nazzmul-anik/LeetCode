f809bf83362e27d07a5141a6ade662d6713d3543
Md. Nazmul Hasan Anik <110289715+nazzmul-anik@users.noreply.github.com>
2024-03-19 00:41:30 +0600

    Time: 0 ms (100%), Space: 41.5 MB (6.61%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,43 +1,41 @@
 class Solution {
 
-    public static int solve(Integer ind, List<Integer> nums, Integer[] dp){
-        if(ind<0) return 0;
-        if(ind == 0) return dp[ind] = nums.get(ind);
+    public static int solve(int ind, List<Integer> num, int[] dp){
+        if(ind < 0) return 0;
+        if(ind == 0){
+            return num.get(ind);
+        }
 
-        if(dp[ind] != null) return dp[ind];
+        if(dp[ind] != -1) return dp[ind];
 
-        int notTake = 0 + solve(ind-1, nums, dp);
-        int take = nums.get(ind) + solve (ind-2, nums, dp);
+        int notTake = 0 + solve(ind-1, num, dp);
+        int take = num.get(ind) + solve(ind-2, num, dp);
 
-        return dp[ind] = Math.max(take, notTake);
+        dp[ind] = Math.max(take, notTake);
+
+        return dp[ind];
     }
 
     public int rob(int[] nums) {
         
         int n = nums.length;
-
         if(n==1) return nums[0];
-
         List<Integer> num1 = new ArrayList<Integer>();
         List<Integer> num2 = new ArrayList<Integer>();
 
         for(int i=0; i<n; i++){
-            if(i!=0){
-                num1.add(nums[i]);
-            }
-            if(i != (n-1)){
-                num2.add(nums[i]);
-            }
+            if(i!=0) num1.add(nums[i]);
+            if(i!= (n-1)) num2.add(nums[i]);
         }
 
-        Integer dp[] = new Integer[n];
-        Arrays.fill(dp, null);
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
 
-        int leaveFirst = solve(n-2, num1, dp);
-        
-        Arrays.fill(dp, null);
-        int leaveLast = solve(n-2, num2, dp);
+        int first = solve(n-2, num1, dp);
+        Arrays.fill(dp, -1);
 
-        return Math.max(leaveFirst, leaveLast);
+        int last = solve(n-2, num2, dp);
+
+        return Math.max(first, last);
     }
 }
