e8a528fadff48f63a2546c377e6ec58c9ef0a278
Md. Nazmul Hasan Anik <110289715+nazzmul-anik@users.noreply.github.com>
2024-12-17 00:59:02 +0600

    Time: 0 ms (100%), Space: 45 MB (79.25%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,15 +1,10 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        
         int n = nums.length;
-        int Xor = 0;
-        for(int i=0; i<=n; i++){
-            Xor = Xor ^ i;
-        }
+        int total = (n * (n+1))/2;
         for(int i=0; i<n; i++){
-            Xor = Xor ^ nums[i];
+            total -= nums[i];
         }
-
-        return Xor;
+        return total;
     }
 }
