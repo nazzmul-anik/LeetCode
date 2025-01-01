409b7349d989f2f8ffd2b845535f4011f855da2a
Md. Nazmul Hasan Anik <110289715+nazzmul-anik@users.noreply.github.com>
2025-01-01 23:38:44 +0600

    Time: 1 ms (97.36%), Space: 41.8 MB (25.32%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
@@ -1,51 +1,21 @@
 class Solution {
+
+    public int max(int a, int b){
+        if(a>b) return a;
+        return b;
+    }
+
     public int maxScore(String s) {
+        
         int n = s.length();
-        int[] zero = new int[n];
-        int[] one = new int[n];
-        boolean minimumOne_1 = false;
-        boolean minimumOne_0 = false;
-
-        for(int i=0; i<n; i++){
-            if(i==0 && s.charAt(i)=='0'){
-                zero[i] = 1;
-                minimumOne_0 = true;
-            }
-            else if(s.charAt(i)=='0'){
-                zero[i] = zero[i-1] + 1;
-                minimumOne_0 = true;
-            }else if(i>0){
-                zero[i] = zero[i-1];
-            }
+        int one = 0, zero = 0;
+        int maxScore = 0;
+        for(int i=0; i<n; i++) if(s.charAt(i)=='1') one++;
+        for(int i=0; i<n-1; i++){
+            if(s.charAt(i)=='1') one--;
+            else zero ++;
+            maxScore = max(maxScore, zero+one);
         }
-
-        for(int i=n-1; i>=0; i--){
-            if(i==n-1 && s.charAt(i)=='1'){
-                one[i] = 1;
-                minimumOne_1 = true;
-            }
-            else if(s.charAt(i)=='1'){
-                one[i] = one[i+1] + 1;
-                minimumOne_1 = true;
-            }else if(i<n-1){
-                one[i] = one[i+1];
-            }
-        }
-
-        int maxSum = 0;
-        int sum;
-        for(int i=0; i<n; i++){
-            sum = zero[i] + one[i];
-            System.out.print(sum + " ");
-            if(sum>maxSum){
-                maxSum = sum;
-            }
-        }
-
-        if(minimumOne_0 == false || minimumOne_1 == false){
-            return maxSum - 1;
-        }
-
-        return maxSum;
+        return maxScore;
     }
 }
