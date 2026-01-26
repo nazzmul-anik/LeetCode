643f417605f8e87147c6d087e45b1cfc36454f95
Md. Nazmul Hasan Anik <110289715+nazzmul-anik@users.noreply.github.com>
2026-01-26 15:06:30 +0600

    Time: 0 ms (100%), Space: 42.4 MB (10.97%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,14 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[46];
-        dp[0]=1;
-         dp[1]=1;
-        if(n==1) return 1;
+        int[] dp = new int[50];
+        dp[1] = 1;
+        dp[2] = 2;
 
-        for(int i=2; i<=n; i++){
-            dp[i] = dp[i-2]+dp[i-1];
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
-
         return dp[n];
     }
 }
